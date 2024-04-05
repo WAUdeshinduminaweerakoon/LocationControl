62c9633f221d896de6183edd0ce91a8c6035a300
WAUdeshinduminaweerakoon <udeshidumina@gmail.com>
2024-04-05 06:50:13 +0530

    connecting the MongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,23 +1,26 @@
-const Model = require("../models/userModel");
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const locationRoutes = require('./routes/locationRoutes');
+const cors = require('cors')
 
-exports.createUser = async (req, res) => {
-    try {
-        const email = req.body.email;
-        const existingUser = await UserModel.findOne({ email: email });
-        if (existingUser) {
-            return res.status(401).json({ message: "User with this email already exists" });
+const app = express();
+app.use(cors())
 
-        }
+app.use(express.json());
+// MongoDB connection
+mongoose.connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("Connected to MongoDB");
+}).catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+});
 
-        const newUser = new UserModel({
-            firstname: req.body.firstname,
-            lastname: req.body.lastname,
-            email: email,
-            password: req.body.password
-        });
-        const savedUser = await newUser.save();
-        res.json(savedUser);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-};
+// Routes
+
+app.use("/location", locationRoutes);
+
+
+module.exports = app;
